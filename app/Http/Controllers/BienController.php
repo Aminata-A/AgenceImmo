368f1107d9711bdae84cc85ec3ce4b6655e81fc1
ndourmouhammad <ndourmouhammad@gmail.com>
2024-06-03 12:35:16 +0000

    ajout de la méthode pour l'affichage des biens

--- app/Http/Controllers/BienController.php
+++ app/Http/Controllers/BienController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Bien;
 use Illuminate\Http\Request;
 
 class BienController extends Controller
 {
-    //
+    public function accueil()
+    {
+        $biens = Bien::all();
+        return view('biens.accueil', compact('biens'));
+    }
 }

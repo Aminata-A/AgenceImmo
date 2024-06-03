c1b0396508e82de3f6cf6a54c605b5bb0eac9302
ndourmouhammad <ndourmouhammad@gmail.com>
2024-06-03 12:32:06 +0000

    ajout de la route accueil

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\BienController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/', [BienController::class, 'accueil'])->name('accueil');
